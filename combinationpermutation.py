2a75c89399321da2551e3006bd5476c26701e81c
kevlu8 <69993704+kevlu8@users.noreply.github.com>
2021-10-15 09:48:50 -0600

    Create combinationpermutation.py

---
+++ combinationpermutation.py
@@ -0,0 +1,21 @@
+# I think I'm a kinesthetic learner, I learned a lot from your mom last night
+
+def factorial(x):
+    result = 1
+    i = x
+    while i <= x and i > 0:
+        result *= i
+        return result
+
+while True:
+    num1 = int(input("What is the first number? "))
+    num2 = int(input("What is the second number? "))
+
+    oper = input("Combination or permutation? ").lower()
+    if oper == "combination":
+        print(factorial(num1) / factorial(num2) * factorial(num1 - num2)
+    elif oper == "permutation":
+        print(factorial(num1) / factorial(num1 - num2)
+    else:
+        print("Invalid!")
+        break
